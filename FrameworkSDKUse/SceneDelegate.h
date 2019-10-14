bfd664508f271d6681459a8535c8d3e933a051f8
杨俊艺 <1209032479@qq.com>
2019-10-14 19:25:41 +0800

    Initial Commit

---
+++ FrameworkSDKUse/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  FrameworkSDKUse
+//
+//  Created by 杨俊艺 on 2019/10/14.
+//  Copyright © 2019 杨俊艺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ FrameworkSDKUse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FrameworkSDKUse
+//
+//  Created by 杨俊艺 on 2019/10/14.
+//  Copyright © 2019 杨俊艺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
